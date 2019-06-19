14c5033c073a6620f0ae1f2a03abe439bf9aeb43
Jihong Kim <jk279242naver.com>
2019-06-18 19:24:50 -0500

    I started with Pseudo Coding for game.js file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// PSEUDO CODING!!!!!!
+// 1. Creates number between 19 and 120 randomly
+    // displays random number on the DOM
+// 2. Creates random number from 1 to 12 for each crystal when clicked
+    // each image should have RANDOM and HIDDEN points from 1 to 12
+// 3. adds up scores from clicking each crystal repeatedly
+// 4. resets the game
+    // a. IF random number === total score, you WIN
+    // b. ELSE IF random number < total score, you LOSE
+// 6. WINS and LOSSES should update as soon as the game resets
